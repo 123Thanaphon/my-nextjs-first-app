d464afe6b6f0013691df23853bd7cc88eff0bbb6
Thanaphon Senarak <thanaphon.s@noblehome.com>
2025-07-18 15:31:11 +0700

    navbar and footer

---
+++ src/components/TheFooter.tsx
@@ -0,0 +1,7 @@
+export default function TheFooter() {
+	return (
+		<footer className="p-4 bg-gray-100 text-center">
+			<p>© 2025 MyStore. All rights reserved.</p>
+		</footer>
+	);
+}
---
+++ src/components/TheNavbar.tsx
@@ -0,0 +1,7 @@
+export default () => {
+	return (
+		<nav className="p-4 bg-white shadow">
+			<div className="container mx-auto">My Logo | Navigation Links</div>
+		</nav>
+	);
+}
