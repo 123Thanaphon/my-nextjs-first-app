8af2d7c5f1aa77c9729e2b11290f5586f81cfc05
Thanaphon Senarak <thanaphon.s@noblehome.com>
2025-07-19 08:59:04 +0700

    TheNavBar

--- src/components/TheNavbar.tsx
+++ src/components/TheNavbar.tsx
@@ -1,7 +1,9 @@
-export default () => {
+const TheNavBar = () => {
 	return (
-		<nav className="p-4 bg-white shadow">
-			<div className="container mx-auto">My Logo | Navigation Links</div>
+		<nav className="p-4 bg-transparent">
+			<div className="container mx-auto text-white font-bold">My Logo | Navigation Links</div>
 		</nav>
 	);
-}
+};
+
+export default TheNavBar;
